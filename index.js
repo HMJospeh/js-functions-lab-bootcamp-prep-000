39aa3b1b48a47d61cd7ef77bb93c5325efb6242f
Humza Joseph <humza1born@gmail.com>
2017-12-12 16:45:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays(){
+  return "happyHolidays";
+} happyHolidays('happyHolidays')
+
+function happyHolidaysTo(name){
+  return"happyHolidays Flatiron";
+} happyHolidaysTo('Flatiron')
+function happyHolidaysTo(holiday, name) {
+  return"Happy ${holiday}, ${name}!";
+} happyHolidaysTo('Independence','Flatiron')
+function holidayCountdown(holiday, day) {
+return"It's ${20} days until ${Mother's day}! ";
+}holidayCountdown('It/'s 20 days until mother's day')
